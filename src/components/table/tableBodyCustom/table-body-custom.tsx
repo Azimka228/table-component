9a2af709114922bbc636e1648431691c9c5921a6
Azimka <71658005+Azimka228@users.noreply.github.com>
2023-01-20 22:50:16 +0300

    refactor: some refactore code

--- src/components/table/tableBodyCustom/table-body-custom.tsx
+++ src/components/table/tableBodyCustom/table-body-custom.tsx
@@ -5,6 +5,7 @@ import {StyledTableCell} from '../../tableStyledComponents/styled-table-cell';
 import {ProductType} from '../../../api/api';
 import {TableBodyRow} from './table-body-row';
 import {ColumnValuesType} from '../tableHeadCustom/table-head-cell';
+import s from './table-body-custom.module.css'
 
 type TableBodyCustomPropsType = {
   rowsPerPage: number;
@@ -13,24 +14,28 @@ type TableBodyCustomPropsType = {
   columns: Array<ColumnValuesType>;
 };
 
+
+
 export const TableBodyCustom: FC<TableBodyCustomPropsType> = ({
-  rowsPerPage,
-  page,
-  rows,
-  columns,
-}) => {
+                                                                rowsPerPage,
+                                                                page,
+                                                                rows,
+                                                                columns,
+                                                              }) => {
   // Avoid a layout jump when reaching the last page with empty rows.
   const emptyRows =
     page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
+  const currentRows = rowsPerPage > 0
+    ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+    : rows;
+
   return (
     <TableBody>
-      {(rowsPerPage > 0
-        ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-        : rows
-      ).map(row => (
-        <TableBodyRow key={row.id} row={row} columns={columns} />
-      ))}
+      {currentRows.length > 0 ?
+        currentRows.map(row => (<TableBodyRow key={row.id} row={row} columns={columns} />))
+        :
+        <div className={s.emptyRows}>Rows is empty</div>}
       {emptyRows > 0 && (
         <StyledTableRow style={{height: 53 * emptyRows}}>
           <StyledTableCell colSpan={6} />
